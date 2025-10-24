9c53c1da5515f5a9fb5e45d5b13528a46614766a
darknet33_PC <roytop10@hotmail.com>
2025-10-24 18:57:13 -0400

    enlaces dinamicos desde la barra de navegacion configurados

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,5 +50,66 @@ def on_startup():
 app.include_router(modules_router)
 
 
-# Sirve index.html desde /
-app.mount("/", StaticFiles(directory=STATIC_DIR, html=True), name="frontend")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.database import Base, engine
+from app.modules import router as modules_router
+from fastapi.staticfiles import StaticFiles
+import os
+from fastapi.responses import FileResponse
+
+# Crea la aplicación FastAPI
+app = FastAPI(
+    title="API Sistema Control Documentos FastAPI",
+    description="Proyecto base con estructura modular.",
+    version="1.0.0"
+)
+
+# Configuración de CORS
+origins = [
+    "http://localhost:5173",  # Frontend en Vite
+    "http://127.0.0.1:5173",   # Alternativa (a veces el navegador usa 127.0.0.1 en lugar de localhost)
+    "http://192.168.0.10:5173",
+    "http://192.168.0.8:5173",
+    "http://192.168.1.9:5173"   # Alternativa (a veces el navegador usa 127.0.0.1 en lugar de localhost)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # Orígenes permitidos
+    allow_credentials=True,
+    allow_methods=["*"],            # Métodos permitidos (GET, POST, PUT, DELETE...)
+    allow_headers=["*"],            # Headers permitidos
+)
+
+# Ruta absoluta a la carpeta 'static' dentro de 'app'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # /path/to/backend/app
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+# Montar la carpeta estática
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+
+# Al iniciar la aplicación, crea las tablas si no existen en 'test.db'
+@app.on_event("startup")
+def on_startup():
+    print("Creando tablas de base de datos si no existen...")
+    Base.metadata.create_all(bind=engine)
+    print("Base de datos lista.")
+
+
+# Incluir todos los routers automáticamente
+app.include_router(modules_router)
+
+
+# Ruta para manejar rutas desconocidas del frontend
+@app.get("/{full_path:path}", include_in_schema=False)
+def spa_routes(full_path: str):
+    # Si el archivo existe en static, lo servimos directamente (por ejemplo assets/*.js o *.css)
+    requested_path = os.path.join(STATIC_DIR, full_path)
+    if os.path.isfile(requested_path):
+        return FileResponse(requested_path)
+
+    # Si no existe el archivo, devolvemos index.html (React Router se encarga)
+    return FileResponse(os.path.join(STATIC_DIR, "index.html"))
+
